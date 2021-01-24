b6c595b63e9a33c0ee783fc097d34cac4bbe94c2
Kristaps Grinbergs <fassko@gmail.com>
2021-01-24 22:11:07 +0200

    first commit

---
+++ TextInCircleSwiftUI/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  TextInCircleSwiftUI
+//
+//  Created by Kristaps Grinbergs on 24/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  private var circleColor = Color(UIColor.systemGray)
+  
+  var body: some View {
+    VStack(spacing: 50) {
+      // With ZStack
+      ZStack {
+        Circle()
+          .stroke(circleColor, lineWidth: 4)
+        
+        Text("13")
+      }
+      .frame(width: 40, height: 40)
+      
+      // Overlay
+      Text("13")
+        .padding()
+        .overlay(
+          Circle()
+            .stroke(circleColor, lineWidth: 4)
+            .padding(6)
+        )
+      
+      // Background
+      Text("13")
+        .padding()
+        .background(
+          Circle()
+            .stroke(circleColor, lineWidth: 4)
+            .padding(6)
+        )
+      
+      // Fill circle usig cliped shape
+      Text("13")
+        .padding()
+        .background(circleColor)
+        .clipShape(Circle())
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      ContentView()
+      
+      ContentView()
+        .preferredColorScheme(.dark)
+    }
+  }
+}
---
+++ TextInCircleSwiftUI/TextInCircleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextInCircleSwiftUIApp.swift
+//  TextInCircleSwiftUI
+//
+//  Created by Kristaps Grinbergs on 24/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TextInCircleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
